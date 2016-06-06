cd20f58676bb29dbaa4aa718d9413f947d2ec83b
Saravanan <saravanaraj73@gmail.com>
2016-06-06 10:54:25 +0530

    Update and rename README.md to str.cpp

---
+++ str.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include<stdlib.h>
+#include<string.h>
+int main() 
+{
+ int num;
+   char m[100];
+   gets(m);
+   int num;
+   num=atoi(m);
+   cout<<num;
+   return (0);
+}
